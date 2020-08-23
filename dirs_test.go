9145b25094425147c3a173c20474aeeedf4c1f40
Daisuke Fujita <dtanshi45@gmail.com>
2020-08-23 17:01:47 +0900

    Add fail case for makeDirs

--- dirs_test.go
+++ dirs_test.go
@@ -1,62 +1,99 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
 func TestMakeDirs(t *testing.T) {
-	dt := &dirTree{
-		"japan": dirTree{
-			"nagoya": nil,
-			"osaka":  nil,
-			"tokyo": dirTree{
-				"shibuya":  nil,
-				"shinjuku": nil,
+	testcases := map[string]struct {
+		dt       *dirTree
+		wantDirs []string
+		wantErr  error
+	}{
+		"success": {
+			dt: &dirTree{
+				"japan": dirTree{
+					"nagoya": nil,
+					"osaka":  nil,
+					"tokyo": dirTree{
+						"shibuya":  nil,
+						"shinjuku": nil,
+					},
+				},
+				"malaysia": dirTree{
+					"kuala_lumpur": nil,
+				},
+				"singapore": nil,
 			},
+			wantDirs: []string{
+				"japan",
+				"japan/nagoya",
+				"japan/osaka",
+				"japan/tokyo",
+				"japan/tokyo/shibuya",
+				"japan/tokyo/shinjuku",
+				"malaysia",
+				"malaysia/kuala_lumpur",
+				"singapore",
+			},
+			wantErr: nil,
 		},
-		"malaysia": dirTree{
-			"kuala_lumpur": nil,
+		"fail": {
+			dt: &dirTree{
+				"japan": "foo",
+			},
+			wantDirs: []string{},
+			wantErr:  fmt.Errorf("is not a valid tree"),
 		},
-		"singapore": nil,
-	}
-	wantDirs := []string{
-		"japan",
-		"japan/nagoya",
-		"japan/osaka",
-		"japan/tokyo",
-		"japan/tokyo/shibuya",
-		"japan/tokyo/shinjuku",
-		"malaysia",
-		"malaysia/kuala_lumpur",
-		"singapore",
 	}
 
-	rootDir, err := ioutil.TempDir("", "TestMakeDirs")
-	if err != nil {
-		t.Fatalf("cannot create tempdir: %s", err)
-	}
-	defer os.RemoveAll(rootDir)
+	for name, tc := range testcases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
 
-	if err := makeDirs(rootDir, dt); err != nil {
-		t.Errorf("want no error, got %s", err)
-	}
+			rootDir, err := ioutil.TempDir("", "TestMakeDirs")
+			if err != nil {
+				t.Fatalf("cannot create tempdir: %s", err)
+			}
+			defer os.RemoveAll(rootDir)
 
-	for _, wd := range wantDirs {
-		dir := filepath.Join(rootDir, wd)
+			err = makeDirs(rootDir, tc.dt)
 
-		if s, err := os.Stat(dir); err != nil {
-			if os.IsNotExist(err) {
-				t.Errorf("dir %q does not exist", dir)
+			if tc.wantErr == nil {
+				for _, wd := range tc.wantDirs {
+					if err != nil {
+						t.Errorf("want no error, got %s", err)
+					}
+
+					dir := filepath.Join(rootDir, wd)
+
+					if s, err := os.Stat(dir); err != nil {
+						if os.IsNotExist(err) {
+							t.Errorf("dir %q does not exist", dir)
+						} else {
+							t.Errorf("unexpected error at checking %q", dir)
+						}
+					} else {
+						if !s.IsDir() {
+							t.Errorf("%q is not a directory", dir)
+						}
+					}
+				}
 			} else {
-				t.Errorf("unexpected error at checking %q", dir)
+				if err == nil {
+					t.Errorf("want error %s, got no error", tc.wantErr)
+				}
+
+				if !strings.Contains(err.Error(), tc.wantErr.Error()) {
+					t.Errorf("want error %q, got %q", tc.wantErr.Error(), err.Error())
+				}
 			}
-		} else {
-			if !s.IsDir() {
-				t.Errorf("%q is not a directory", dir)
-			}
-		}
+		})
 	}
 }
